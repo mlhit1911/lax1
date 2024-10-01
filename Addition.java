c403726146a3d4952da7705460ecbcef1286bfb2
mlhit1911 <lakshumaiah@klh.edu.in>
2024-10-01 13:47:15 +0530

    hello

---
+++ Addition.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Addition
+{
+public static void main(String args[])
+{
+System.out.println("welcome to java");
+  int a=10,b=20,c;
+  c=a+b;
+  System.out.println(c);
+}
+}
